5ef4e0045a1c7f6a8ad7d73f7fac266c112bc2eb
jakum <jakum@DESKTOP-28BPS4J>
2024-06-25 14:04:28 +0900

    start project

---
+++ jkproject/src/main/java/com/example/jkproject/JkprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.jkproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkprojectApplication.class, args);
+	}
+
+}
---
+++ jkproject/src/test/java/com/example/jkproject/JkprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jkproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
